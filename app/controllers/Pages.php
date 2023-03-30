6d29aad2aef9ac8c15a5689728866c292f36456f
5022230021 <Naweeparb.Wangwanich@ap.sony.com>
2023-03-30 18:21:16 +0700

    Update complete structure

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,27 +3,25 @@
 
         public function __construct()
         {
-            // echo 'Pages loaded';
-            // echo '<br>';
             $this->postModel = $this->model('Post');
         }
 
         public function index(){
-            // echo 'index';
-            // $this->view('hello');
+
             $posts = $this->postModel->getPosts();
 
             $data = [
                 'title' => 'Welcome',
                 'posts' => $posts
             ];
-            // print_r($data['posts']);
             
             $this->view('pages/index', $data);
         }
 
         public function about(){
-            // echo $id;
-            $this->view('pages/about');
+            $data = [
+                'title' => 'About us',
+            ];
+            $this->view('pages/about', $data);
         }
     }
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,1 +1,27 @@
-HEADER
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo SITENAME ?></title>
+
+    <link rel="stylesheet" href="../../../../vendor/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../../vendor/fontawesome-free-6.3.0-web/css/all.min.css">
+    <link rel="stylesheet" href="../../../../vendor/sweetalert2-11.7.3/package/dist/sweetalert2.min.js">
+    <link rel="icon" href="../../../vendor/favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+    <link rel="stylesheet" href="../../../../vendor/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #C9EEFF;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">STT_EXPORT</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+    <div class="container-fluid">
